5c8fbe3b4a455f8564c004e4ed70d6f059c87a81
Aleksandr <patachyon@gmail.com>
2022-03-20 15:33:40 +0300

    feat: Add triangle class

---
+++ Shapes/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Triangle : Shape
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public override double Area => Math.Sqrt(
+            HalfPerimeter *
+            (HalfPerimeter - A) *
+            (HalfPerimeter - B) *
+            (HalfPerimeter - C));
+        public double Perimeter => A + B + C;
+
+        private double HalfPerimeter => Perimeter / 2;
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+    }
+}
