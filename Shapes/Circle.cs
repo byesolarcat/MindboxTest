64cfb7669f43f9e61f700a17f3e774c5df8a46d6
Aleksandr <patachyon@gmail.com>
2022-03-20 16:53:54 +0300

    feat: Add circle class

---
+++ Shapes/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double Area => Math.PI * Math.Pow(Radius, 2);
+
+        public Circle(double radius)
+        {
+            if (radius <= 0) 
+            {
+                throw new ArgumentOutOfRangeException(nameof(radius), "Radius must be bigger than zero.");
+            }
+
+            Radius = radius;
+        }
+    }
+}
